e29d669805c1709c91fa0a21918030cc7eefb171
Rahij Ramsharan <rahij.ramsharan@gmail.com>
2016-03-10 13:27:58 +0700

    throw ArgumentErrors instead of general Exception

--- parking.rb
+++ parking.rb
@@ -39,42 +39,45 @@ end
 
 def slot_number_for_registration_number(plate:)
   slot_num = @lot.cars.index {|slot| slot.plate == plate}
-  slot_num = slot_num + 1 unless slot_num.nil?
-  slot_num
+  if slot_num.nil?
+    "Not found"
+  else
+    slot_num + 1
+  end
 end
 
 def parse_and_execute(tokens)
   case tokens.first
 
   when 'create_parking_lot'
-    raise INVALID_NUM_SLOTS_ERROR unless (tokens[1].to_i > 0)
+    raise ArgumentError, INVALID_NUM_SLOTS_ERROR unless (tokens[1].to_i > 0)
     create_parking_lot(size: tokens[1].to_i)
 
   when 'park'
-    raise EMPTY_PLATE_NUMBER_COLOR_ERROR unless tokens.size >= 3
+    raise ArgumentError, EMPTY_PLATE_NUMBER_COLOR_ERROR unless tokens.size >= 3
     park(plate: tokens[1], color: tokens[2])
 
   when 'leave'
-    raise INVALID_SLOT_NUMBER_ERROR unless (tokens[1].to_i > 0)
+    raise ArgumentError, INVALID_SLOT_NUMBER_ERROR unless (tokens[1].to_i > 0)
     leave(slot: tokens[1].to_i)
 
   when 'status'
     status
 
   when 'registration_numbers_for_cars_with_colour'
-    raise EMPTY_COLOR_ERROR unless tokens.size >= 2
+    raise ArgumentError, EMPTY_COLOR_ERROR unless tokens.size >= 2
     registration_numbers_for_cars_with_colour(color: tokens[1])
 
   when 'slot_numbers_for_cars_with_colour'
-    raise EMPTY_COLOR_ERROR unless tokens.size >= 2
+    raise ArgumentError, EMPTY_COLOR_ERROR unless tokens.size >= 2
     slot_numbers_for_cars_with_colour(color: tokens[1])
 
   when 'slot_number_for_registration_number'
-    raise EMPTY_COLOR_ERROR unless tokens.size >= 2
+    raise ArgumentError, EMPTY_COLOR_ERROR unless tokens.size >= 2
     slot_number_for_registration_number(plate: tokens[1])
 
   else
-    raise INVALID_COMMAND_ERROR
+    raise ArgumentError, INVALID_COMMAND_ERROR
   end
 end
 
@@ -99,7 +102,11 @@ def init_shell
   while(true) do
     print "> "
     command = STDIN.readline
-    process_command(command)
+    begin
+      process_command(command)
+    rescue ArgumentError => e
+      print_response(e.message)
+    end
   end
 end
 
